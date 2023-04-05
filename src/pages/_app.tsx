e2e97faadfcbfa278a25de836b4acb0e72ad3932
Radu Turtoi <radu.turtoi@gmail.com>
2023-04-05 12:56:28 +0300

    layout component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css"
+import { AnimatePresence } from 'framer-motion'
+import type { AppProps } from "next/app"
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps, router }: AppProps) {
+  console.log(Component)
+  return (
+    <AnimatePresence mode="wait">
+      <Component {...pageProps} key={router.asPath}/>
+    </AnimatePresence>
+  )
 }
