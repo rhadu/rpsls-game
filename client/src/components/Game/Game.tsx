731b0e4e661a5d8cdd81994c45557ebf44751c4d
Radu Turtoi <radu.turtoi@gmail.com>
2023-04-07 15:39:24 +0300

    Game component

---
+++ client/src/components/Game/Game.tsx
@@ -0,0 +1,10 @@
+
+  import React from 'react'
+
+  export interface IGameProps { }
+
+  const Game = ({}: IGameProps) => {
+    return <div>Hello , I am a Game component.</div>;
+  };
+
+  export default Game;
