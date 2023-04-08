678379622a8d19ad27bb225b8430eb6267794cae
Radu Turtoi <radu.turtoi@gmail.com>
2023-04-08 08:37:33 +0300

    [client] waiting room design

--- client/src/components/WaitingRoom/WaitingRoom.tsx
+++ client/src/components/WaitingRoom/WaitingRoom.tsx
@@ -1,21 +1,35 @@
 import { motion } from "framer-motion"
 import React from "react"
 import Logo from "../Logo"
-import Spacer from '../Spacer'
+import Spacer from "../Spacer"
+import useGameState from "@/store/game"
+import { GameState } from "@/types/game"
 
 export interface IWaitingRoomProps {}
 
 const WaitingRoom = ({}: IWaitingRoomProps) => {
+  const { setGameState } = useGameState((state) => ({
+    setGameState: state.setGameState,
+  }))
+
+  React.useEffect(() => {
+    const timeoutId = window.setTimeout(() => {
+      console.log("Waiting Room -> Matchup")
+      setGameState(GameState.MATCHUP_INTRO)
+    }, 1000)
+    return () => {
+      window.clearTimeout(timeoutId)
+    }
+  }, [setGameState])
   return (
-    <motion.main className="flex flex-col items-center">
-      <motion.div layoutId="logo" className="py-10">
+    <motion.div className="flex flex-col items-center h-full">
+      <motion.header className="py-10">
         <Logo width={100} />
-      </motion.div>
-      <Spacer size={"200px"} />
+      </motion.header>
 
       <motion.div
         initial={{
-          y: 10,
+          y: 20,
           opacity: 0,
         }}
         animate={{
@@ -23,13 +37,14 @@ const WaitingRoom = ({}: IWaitingRoomProps) => {
           opacity: 1,
         }}
         exit={{
-          y: -10,
+          y: 20,
           opacity: 0,
         }}
+        className="py-40 text-3xl font-bold text-yellow-300 "
       >
-        Hello , I am a WaitingRoom component.
+        Waiting for Opponent ...
       </motion.div>
-    </motion.main>
+    </motion.div>
   )
 }
 
